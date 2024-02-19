4a333466e31fe59db0449f1d7797326a251882bb
Josh Mayer <jooshmayer@gmail.com>
2024-02-18 22:55:41 -0500

    new homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <header className="">
-            <div className="fixed top-0 flex justify-between w-full dark:mix-blend-difference px-4 sm:px-10 pt-8 font-bold tracking-tighter">
+            <div className="fixed top-0 flex justify-between w-full dark:mix-blend-difference px-4 sm:px-10 pt-8 font-bold">
               <nav className="text-md space-x-6 underline">
                 <Link href="/">Home</Link>
                 <Link href="/posts">Notes</Link>
@@ -41,10 +41,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <Analytics />
 
           <footer>
-            <div className="fixed bottom-0 w-full px-4 sm:px-10 pb-8 dark:mix-blend-difference font-bold tracking-tighter">
+            <div className="fixed bottom-0 w-full px-4 sm:px-10 pb-8 dark:mix-blend-difference font-bold">
               <nav className="text-sm flex justify-between space-x-6">
                 <div className="">
-                  <h3 className="">Josh Mayer, 2023.</h3>
+                  <h3 className="">Josh Mayer, 2024. San Francisco.</h3>
                 </div>
                 <div className="space-x-6 underline">
                   <Link href="https://x.com/jooshmayer">X</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,59 @@ import { NextResponse } from "next/server";
 
 export default function Home() {
   return (
-    <div className="max-w-3xl prose dark:prose-invert mt-16">
+    <div className="max-w-3xl prose dark:prose-invert mt-8">
       <div className="">
-        <h3 className="">Hello, my name is Josh Mayer.</h3>
+        <h1 className="my-0 font-extrabold tracking-tighter">Josh Mayer</h1>
         <p>
-          I am a textbook internet native/addict, which has led me to try
-          building my own parts of it. Sometimes for school, myself, youtube, or
-          real projects.
+          I'm obsessed with building an agent-run internet, writing passing Go
+          tests, and the GSW.
         </p>
+
+        <h3>Currently Working On</h3>
+        <ul>
+          <li>
+            Building real-time data infrastructure for an internet without
+            websites. Founding team with{" "}
+            <Link href={"https://tollbit.com"}>tollbit.com</Link>. We just
+            raised 6M.
+          </li>
+          <li>
+            Some <Link href={"/posts"}>writing</Link> (like every tech bro)
+          </li>
+        </ul>
+
+        <h3>Past Work</h3>
+        <ul>
+          <li>
+            Built <Link href={"https://sunflowers.dev"}>sunflowers.dev</Link>.
+            An internship application spray and pray bot for over 8,000 students
+            in two months. <span className="text-primary">(Aquired)</span>
+          </li>
+          <li>
+            Built{" "}
+            <Link href={"https://top.gg/bot/516340661247541248"}>
+              Scrimius Bot
+            </Link>
+            . A Discord bot with 100,000 users that paired Fortnite gamers
+            together in the same match.{" "}
+            <span className="text-primary">(Oldie but goodie)</span>
+          </li>
+        </ul>
+
+        <h3>Other</h3>
+        <ul>
+          <li>
+            Worked at <Link href={"https://sidelinehd.com"}>Sideline HD</Link>{" "}
+            and built their volleyball scoring application that serves over
+            3,000 high school teams.
+          </li>
+          <li>
+            Tried (but lost) to build{" "}
+            <Link href={"https://getbeme.com"}>Beme</Link> a phone to phone
+            contact sharing service like Bump. Using updated technology like
+            BLE.
+          </li>
+        </ul>
       </div>
     </div>
   );
